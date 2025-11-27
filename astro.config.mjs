3a66d9fa1c28d27b3c489c2f70a8847d200592b1
Uhshoi1206 <108212291+Uhshoi1206@users.noreply.github.com>
2025-11-27 22:42:35 +0700

    Updated astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import netlify from '@astrojs/netlify';
 export default defineConfig({
   integrations: [react()],
   output: 'server',
-  adapter: netlify(),
+  adapter: netlify({
+    edgeMiddleware: false,
+  }),
 });
